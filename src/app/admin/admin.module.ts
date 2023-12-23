c3c161994ec5020c0abb7c6e104c9788c9386ba7
cakiciozgur <cakiciozgur@outlook.com>
2023-12-23 22:52:13 +0300

    material, bootstrap, alertify implementation

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from './layout/layout.module';
 import { ComponentsModule } from './components/components.module';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 
--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+
+declare var alertify: any
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit
+{
+  constructor() { }
 
+  ngOnInit(): void {
+    alertify.success('Im ready!');
+  }
 }
