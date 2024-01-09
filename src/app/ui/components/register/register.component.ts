6ae18b832b632d715a94a41786a6728822b47d3e
cakiciozgur <cakiciozgur@outlook.com>
2024-01-09 16:43:56 +0300

    user register service created

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
-import { debug } from 'console';
 import { User } from '../../../entities/user';
+import { UserService } from '../../../services/common/models/user.service';
+import { Create_User } from '../../../contracts/users/create_user';
+import { CustomeToastrService, ToastrMessageType, ToastrPosition } from '../../../services/ui/custome-toastr.service';
+import { MessageType, Position } from '../../../services/admin/alertify.service';
+import { debug } from 'util';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +15,7 @@ import { User } from '../../../entities/user';
 export class RegisterComponent implements OnInit {
   kullaniciForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private userService: UserService, private toastrService: CustomeToastrService) { }
 
   ngOnInit() {
     this.createForm();
@@ -19,30 +23,36 @@ export class RegisterComponent implements OnInit {
 
   createForm() {
     this.kullaniciForm = this.fb.group({
-      adSoyad: ['', [Validators.required,
+      nameSurname: ['', [Validators.required,
                      Validators.maxLength(50),
                      Validators.minLength(3)]],
-      kullaniciAdi: ['', [Validators.required,
+      username: ['', [Validators.required,
                             Validators.maxLength(50),
                             Validators.minLength(3)]],
-      eposta: ['', [Validators.required,
+      email: ['', [Validators.required,
                     Validators.email,
                     Validators.maxLength(250)]],
-      sifre: ['', [Validators.required,
+      password: ['', [Validators.required,
                    Validators.maxLength(50),
                    Validators.minLength(3)]],
-      sifreTekrar: ['', [Validators.required,
+      passwordConfirm: ['', [Validators.required,
                          Validators.maxLength(50),
         Validators.minLength(3)]],
 
     }, { validators: this.passwordMatchValidator() });
   }
 
-  submitForm(data: User) {
+  async submitForm(data: User) {
     if (this.kullaniciForm.valid) {
-      console.log('Form gönderildi', this.kullaniciForm.value);
-      // Burada formu sunucuya gönderme işlemleri yapılabilir.
-    } else {
+      const result: Create_User = await this.userService.create(data);
+      if (result.success) {
+        this.toastrService.message(result.message, "Başarılı", { messageType: ToastrMessageType.Success, position: ToastrPosition.TopRight })
+      }
+      else {
+        this.toastrService.message(result.message, "Hatalı", { messageType: ToastrMessageType.Error, position: ToastrPosition.TopRight })
+      }
+    }
+    else{
       console.log('Form geçerli değil', this.kullaniciForm.value);
     }
   }
@@ -59,8 +69,8 @@ export class RegisterComponent implements OnInit {
 
   passwordMatchValidator(): ValidatorFn {
     return (control: FormGroup): { [key: string]: boolean } | null => {
-      const password = control.get('sifre')?.value;
-      const confirmPassword = control.get('sifreTekrar')?.value;
+      const password = control.get('password')?.value;
+      const confirmPassword = control.get('passwordConfirm')?.value;
 
       return password === confirmPassword ? null : { 'passwordMismatch': true };
     };
